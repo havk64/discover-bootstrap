b707ee7608e2de3bcea5e4324be837c69d16cfc4
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-27 20:55:44 -0300

    Improving formatting of JS file

--- js/popover.js
+++ js/popover.js
@@ -1,2 +1,8 @@
-var options = ({trigger:"hover", placement: "right", content: "Tons of information about Acme's products and services. Click now, you wont be disappointed", title: "What's behind that link" })
+var options = ({
+    trigger:"hover",
+    placement: "right", 
+    content: "Tons of information about Acme's products and services. Click now, you wont be disappointed", 
+    title: "What's behind that link"
+})
+
 $('#Learn-More').popover(options);
