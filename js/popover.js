eabc0fae573ab427e984b9af83d1bae5680a559a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-27 20:52:40 -0300

    Inserting right text to popover

--- js/popover.js
+++ js/popover.js
@@ -1,2 +1,2 @@
-var options = ({trigger:"hover", placement: "right", content: "Some content here",  })
+var options = ({trigger:"hover", placement: "right", content: "Tons of information about Acme's products and services. Click now, you wont be disappointed", title: "What's behind that link" })
 $('#Learn-More').popover(options);
