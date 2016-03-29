e9d3cda82482b592daae0efd992d5e8afb4596c3
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-29 20:31:45 -0300

    Adding default value, Bootstrap colors

--- js/behavior.js
+++ js/behavior.js
@@ -25,7 +25,16 @@ document.addEventListener('DOMContentLoaded', function(){
     });
     
     // => Adding color picker to input element.
-    $('.colorpicker-element').colorpicker().on('changeColor.colorpicker', function(event){
+    $('.colorpicker-element').colorpicker({
+        colorSelectors: {
+            'default': '#777777',
+            'primary': '#337ab7',
+            'success': '#5cb85c',
+            'info': '#5bc0de',
+            'warning': '#f0ad4e',
+            'danger': '#d9534f'
+        }
+    }).on('changeColor.colorpicker', function(event){
         $('body').css('background-color', event.color.toHex());
     });
 })
