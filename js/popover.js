da5292595965d3bbde7497c801a13db21dcc8db9
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-29 15:12:09 -0300

    Adding html markup to star review

--- js/popover.js
+++ js/popover.js
@@ -1,8 +1,9 @@
-var options = ({ // => Options to be added to popover.
-    trigger:"hover",
-    placement: "right",
-    title: "What's behind that link",
-    content: "Tons of information about Acme's products and services. Click now, you wont be disappointed"
+document.addEventListener('DOMContentLoaded', function(){
+    var options = ({ // => Options to be added to popover.
+        trigger:"hover",
+        placement: "right",
+        title: "What's behind that link",
+        content: "Tons of information about Acme's products and services. Click now, you wont be disappointed"
+    })
+    $('#Learn-More').popover(options); // => Call to popover on "hover".
 })
-
-$('#Learn-More').popover(options); // => Call to popover on "hover".
